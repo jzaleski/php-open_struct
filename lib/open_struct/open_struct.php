2030d35defc824a39e41338ead53845906c9297d
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2016-08-26 10:14:33 -0400

    Finish `isset` and `unset` support plus clean-up
    * Rename `$attributes` to `$__attributes` to avoid collision
    * Implement `__isset` (calls `offsetExists`)
    * Implement `__unset` (calls `offsetUnset`)
    * Add more test-cases
    * Add DOC comments to test-cases

--- lib/open_struct/open_struct.php
+++ lib/open_struct/open_struct.php
@@ -2,19 +2,20 @@
 /**
  * A very flexible data-structure (inspired by Ruby's `ostruct` library)
  *
- * PHP version 5+
+ * PHP Version 5+
  *
  * @author Jonathan W. Zaleski <JonathanZaleski@gmail.com>
  */
 
 class Open_Struct implements \ArrayAccess {
-
   /**
-   * Attributes
+   * The associative array of attribute keys/values
    *
-   * @var array an associative array of attribute keys/values
+   * @var array
+   *
+   * @access private
    */
-  private $attributes;
+  private $__attributes;
 
   /**
    * Constructor
@@ -22,32 +23,11 @@ class Open_Struct implements \ArrayAccess {
    * @param array $attributes an associative array of attribute keys/values
    *
    * @return void
+   *
+   * @access public
    */
   public function __construct($attributes = null) {
-    $this->attributes = $this->structify($attributes ?: []);
-  }
-
-  /**
-   * Read attribute
-   *
-   * @param mixed $key an attribute key
-   *
-   * @return mixed the corresponding attribute value (if it exists) or null
-   */
-  public function __get($key) {
-    return $this->offsetGet($key);
-  }
-
-  /**
-   * Write attribute
-   *
-   * @param mixed $key   an attribute key
-   * @param mixed $value an attribute value
-   *
-   * @return void
-   */
-  public function __set($key, $value) {
-    $this->offsetSet($key, $value);
+    $this->__attributes = $this->structify($attributes ?: []);
   }
 
   /**
@@ -56,43 +36,104 @@ class Open_Struct implements \ArrayAccess {
    * @param mixed $key an attribute key
    *
    * @return bool whether or not the specified key exists
+   *
+   * @access public
    */
-  public function offsetExists($key) {
-    return isset($this->attributes[$key]);
+  public function __isset($key) {
+    return $this->offsetExists($key);
   }
 
   /**
-   * Read attribute
+   * Read an attribute
    *
    * @param mixed $key an attribute key
    *
    * @return mixed the corresponding attribute value (if it exists) or null
+   *
+   * @access public
    */
-  public function offsetGet($key) {
-    return $this->offsetExists($key) ? $this->attributes[$key] : null;
+  public function __get($key) {
+    return $this->offsetGet($key);
   }
 
   /**
-   * Write attribute
+   * Write an attribute
    *
    * @param mixed $key   an attribute key
    * @param mixed $value an attribute value
    *
    * @return void
+   *
+   * @access public
    */
-  public function offsetSet($key, $value) {
-    $this->attributes[$key] = $this->structify($value);
+  public function __set($key, $value) {
+    $this->offsetSet($key, $value);
   }
 
   /**
-   * Remove attribute
+   * Remove an attribute
    *
    * @param mixed $key an attribute key
    *
    * @return void
+   *
+   * @access public
+   */
+  public function __unset($key) {
+    $this->offsetUnset($key);
+  }
+
+  /**
+   * Determine if a given attribute key exists
+   *
+   * @param mixed $key an attribute key
+   *
+   * @return bool whether or not the specified key exists
+   *
+   * @access public
+   */
+  public function offsetExists($key) {
+    return isset($this->__attributes[$key]);
+  }
+
+  /**
+   * Read an attribute
+   *
+   * @param mixed $key an attribute key
+   *
+   * @return mixed the corresponding attribute value (if it exists) or null
+   *
+   * @access public
+   */
+  public function offsetGet($key) {
+    return $this->offsetExists($key) ? $this->__attributes[$key] : null;
+  }
+
+  /**
+   * Write an attribute
+   *
+   * @param mixed $key   an attribute key
+   * @param mixed $value an attribute value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function offsetSet($key, $value) {
+    $this->__attributes[$key] = $this->structify($value);
+  }
+
+  /**
+   * Remove an attribute
+   *
+   * @param mixed $key an attribute key
+   *
+   * @return void
+   *
+   * @access public
    */
   public function offsetUnset($key) {
-    unset($this->attributes[$key]);
+    unset($this->__attributes[$key]);
   }
 
   /**
@@ -101,6 +142,8 @@ class Open_Struct implements \ArrayAccess {
    * @param mixed $value anything
    *
    * @return bool whether or not the specified value is an associative-array
+   *
+   * @access private
    */
   private function is_associative_array($value) {
     return is_array($value) && array_keys($value) !== range(0, count($value) - 1);
@@ -112,6 +155,8 @@ class Open_Struct implements \ArrayAccess {
    * @param mixed $value anything
    *
    * @return bool whether or not the specified value is a list
+   *
+   * @access private
    */
   private function is_list($value) {
     return is_array($value) && array_keys($value) === range(0, count($value) - 1);
@@ -123,12 +168,14 @@ class Open_Struct implements \ArrayAccess {
    * @param mixed $value anything
    *
    * @return mixed the value itself, an `Open_Struct` or a list of values (potentially containing `Open_Struct` instances)
+   *
+   * @access private
    */
   private function structify($value) {
     if (empty($value)) {
       return $value;
     } elseif ($this->is_associative_array($value)) {
-      return array_reduce(array_keys($value), function ($memo, $key) use ($value) { $memo[$key] = $this->structify($value[$key]); return $memo; }, new Open_Struct());
+      return array_reduce(array_keys($value), function ($memo, $key) use ($value) { $memo[$key] = $this->structify($value[$key]); return $memo; }, new Open_Struct);
     } elseif ($this->is_list($value)) {
       return array_map(function ($value) { return $this->structify($value); }, $value);
     } else {
--- test/open_struct_test.php
+++ test/open_struct_test.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * `Open_Struct` test-harness
+ *
+ * PHP Version 5+
+ *
+ * @author Jonathan W. Zaleski <JonathanZaleski@gmail.com>
+ */
 
 require_once __DIR__ . '/../lib/open_struct.php';
 
 class Open_Struct_Test extends \PHPUnit_Framework_TestCase {
-
+  /**
+   * Test assigning a simple-key and simple-value
+   *
+   * @return void
+   *
+   * @access public
+   */
   public function test_assigning_simple_key_and_simple_value() {
     $subject = new Open_Struct();
 
@@ -12,6 +25,13 @@ class Open_Struct_Test extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($subject->foo, 1);
   }
 
+  /**
+   * Test assigning a simple-key and callback-value
+   *
+   * @return void
+   *
+   * @access public
+   */
   public function test_assigning_simple_key_and_callback_value() {
     $subject = new Open_Struct();
 
@@ -20,7 +40,14 @@ class Open_Struct_Test extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(call_user_func($subject->foo), 1);
   }
 
-  public function test_assigning_simple_key_and_dict_value() {
+  /**
+   * Test assigning a simple-key and dictionary-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_assigning_simple_key_and_dictionary_value() {
     $subject = new Open_Struct();
 
     $subject->foo = ['bar' => 1];
@@ -28,6 +55,13 @@ class Open_Struct_Test extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($subject->foo->bar, 1);
   }
 
+  /**
+   * Test assigning a simple-key and list-value
+   *
+   * @return void
+   *
+   * @access public
+   */
   public function test_assigning_simple_key_and_list_value() {
     $subject = new Open_Struct();
 
@@ -36,49 +70,193 @@ class Open_Struct_Test extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($subject->foo, [1]);
   }
 
-  public function test_simple_key_and_simple_value() {
+  /**
+   * Test reading a simple-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_reading_simple_value() {
     $subject = new Open_Struct(['foo' => 1]);
 
     $this->assertEquals($subject->foo, 1);
   }
 
-  public function test_simple_key_and_callback_value() {
+  /**
+   * Test reading a callback-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_reading_callback_value() {
     $subject = new Open_Struct(['foo' => function() { return 1; }]);
 
     $this->assertEquals(call_user_func($subject->foo), 1);
   }
 
-  public function test_simple_key_and_dict_value() {
+  /**
+   * Test reading a dictionary-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_reading_dictionary_value() {
     $subject = new Open_Struct(['foo' => ['bar' => 1]]);
 
     $this->assertEquals($subject->foo->bar, 1);
   }
 
-  public function test_simple_key_and_list_value() {
+  /**
+   * Test reading a list-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_reading_list_value() {
     $subject = new Open_Struct(['foo' => [1]]);
 
     $this->assertEquals($subject->foo, [1]);
   }
 
-  public function test_indexing_by_simple_key() {
+  /**
+   * Test indexing a simple-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_indexing_simple_value() {
     $subject = new Open_Struct(['foo' => 1]);
 
-    $this->assertEquals($subject->foo, $subject['foo']);
+    $this->assertEquals($subject['foo'], 1);
   }
 
+  /**
+   * Test indexing a callback-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_indexing_callback_value() {
+    $subject = new Open_Struct(['foo' => function() { return 1; }]);
+
+    $this->assertEquals(call_user_func($subject['foo']), 1);
+  }
+
+  /**
+   * Test indexing a dictionary-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_indexing_dictionary_value() {
+    $subject = new Open_Struct(['foo' => ['bar' => 1]]);
+
+    $this->assertEquals($subject['foo']['bar'], 1);
+  }
+
+  /**
+   * Test indexing a list-value
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_indexing_list_value() {
+    $subject = new Open_Struct(['foo' => [1]]);
+
+    $this->assertEquals($subject['foo'], [1]);
+  }
+
+  /**
+   * Test isset by an index
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_isset_by_index() {
+    $subject = new Open_Struct(['foo' => 1]);
+
+    $this->assertTrue(isset($subject['foo']));
+  }
+
+  /**
+   * Test isset by a simple-key
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_isset_by_simple_key() {
+    $subject = new Open_Struct(['foo' => 1]);
+
+    $this->assertTrue(isset($subject->foo));
+  }
+
+  /**
+   * Test unsetting a top-level key
+   *
+   * @return void
+   *
+   * @access public
+   */
   public function test_unsetting_a_top_level_key() {
     $subject = new Open_Struct(['foo' => 1]);
 
-    unset($subject['foo']);
+    unset($subject->foo);
 
     $this->assertNull($subject->foo);
   }
 
+  /**
+   * Test unsetting a top-level key by index
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_unsetting_a_top_level_key_by_index() {
+    $subject = new Open_Struct(['foo' => 1]);
+
+    unset($subject['foo']);
+
+    $this->assertNull($subject['foo']);
+  }
+
+  /**
+   * Test unsetting a nested key
+   *
+   * @return void
+   *
+   * @access public
+   */
   public function test_unsetting_a_nested_key() {
     $subject = new Open_Struct(['foo' => ['bar' => 1]]);
 
-    unset($subject->foo['bar']);
+    unset($subject->foo->bar);
 
     $this->assertNull($subject->foo->bar);
+  }
+
+  /**
+   * Test unsetting a nested key by index
+   *
+   * @return void
+   *
+   * @access public
+   */
+  public function test_unsetting_a_nested_key_by_index() {
+    $subject = new Open_Struct(['foo' => ['bar' => 1]]);
+
+    unset($subject['foo']['bar']);
+
+    $this->assertNull($subject['foo']['bar']);
   }
 }
